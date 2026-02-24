5d58b1cccbf8617b4e869015770b37c684b1f9be
Rowanleo30 <rfemampilly@gmail.com>
2026-02-24 14:03:03 +0530

    Update pollinationsApi.ts

--- client/src/api/pollinationsApi.ts
+++ client/src/api/pollinationsApi.ts
@@ -1,15 +1,15 @@
 import axios, { AxiosInstance } from "axios"
 
-const pollinationsBaseUrl = "https://enter.pollinations.ai"
+const pollinationsBaseUrl = "https://text.pollinations.ai"
 
 const instance: AxiosInstance = axios.create({
     baseURL: pollinationsBaseUrl,
     headers: {
         "Content-Type": "application/json",
-        "Authorization": "sk_9jsxzXcPVPjHYZAI8cC8NUCApWZzGmFJ"
     },
 })
 
 export default instance
+
 
 
