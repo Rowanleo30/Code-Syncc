e7b94af5b40a98087b82b7fa3d4f454aa3f992d5
Rowanleo30 <rfemampilly@gmail.com>
2026-02-24 13:49:11 +0530

    Update pistonApi.ts

--- client/src/api/pistonApi.ts
+++ client/src/api/pistonApi.ts
@@ -1,12 +1,13 @@
-import axios, { AxiosInstance } from "axios"
+import axios, { AxiosInstance } from "axios";
 
-const pistonBaseUrl = "https://emkc.org/api/v2/piston"
+const pistonBaseUrl = "https://emkc.org/api/v2/piston";
 
 const instance: AxiosInstance = axios.create({
-    baseURL: pistonBaseUrl,
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
+  baseURL: pistonBaseUrl,
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "ae1dd507-6081-461c-b174-d0e8de9584f4" // drop the key directly here
+  },
+});
 
-export default instance
+export default instance;
